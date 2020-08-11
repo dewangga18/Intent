b4dbec1ee4ea69c55418bcc8d3742635e948eb44
Aaron <aaronevanjulio18@gmail.com>
2020-08-11 12:55:18 +0700

    Initial commit

---
+++ app/src/main/java/www/smktelkom/sch/id/intent/MoveActivity.java
@@ -0,0 +1,27 @@
+package www.smktelkom.sch.id.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MoveActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+        Button btnMo = findViewById(R.id.btnmovea);
+        btnMo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnmovea){
+            Intent newIn = new Intent(MoveActivity.this, MoveActivityA.class);
+            startActivity(newIn);
+        }
+    }
+}
